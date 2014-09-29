2da83a46880aa5c484d1b27f826ba8e193040be6
Taddy <mtaddy>
2014-09-29 10:10:05 -0700

    new fgl plots done

--- code/fgl.R
+++ code/fgl.R
@@ -1,9 +1,18 @@
 
 library(distrom)
 library(MASS)
+library(glmnet)
+
 data(fgl)
 fit <- dmr(NULL, fgl[,1:9], fgl$type, 
 	lambda.min.ratio=1e-4, gamma=0, tol=1e-8)
+
+## plot the conditional MLE mu (which we've pegged at zero)
+muhat <- log(1/rowSums(exp(predict(fit,fgl[,1:9]))))
+pdf(file="fgl_mu.pdf", width=4,height=3)
+par(mai=c(.9,.9,.2,.2))
+hist(muhat,main="",xlab="Condtional MLE for mu",col="grey80")
+dev.off()
 
 ## plot the individual Poisson model fit and selection
 pdf(file="fgl_coef.pdf",width=8,height=4)
@@ -17,51 +26,41 @@ mtext("coefficient",outer=TRUE, font=3,side=2,line=1)
 dev.off()
 
 ##### OOS experiment
-lambda <- fit[[1]]$lambda
 K <- 20
 n <- nrow(fgl)
 chunks <- round(seq.int(1,n,length=K+1))
 randi <- sample.int(n)
-oosdev <- rep(0,K)
+oosdev <- as.data.frame(matrix(0,ncol=5,nrow=K))
+names(oosdev) <- c("net.CV1se","net.CVmin","dmr.CV1se","dmr.CVmin","dmr.AICc")
 
-getdev <- function(f){ # multinomial deviance
-			eta <- predict(f,fgl[lo,1:9])
+getdev <- function(f, lo, ...){ # multinomial deviance
+			eta <- drop(predict(f,as.matrix(fgl[lo,1:9]), ...))
 			ylo <- cbind(1:length(lo),fgl$type[lo])
 			d <- eta[ylo] - log(rowSums(exp(eta))) 
 			return(mean(-2*d)) }
 
 for(k in 1:K){ # CV loop
 	lo <- randi[chunks[k]:chunks[k+1]]
-	fitk <- dmr(NULL, 
-				counts=fgl$type[-lo], fgl[-lo,1:9], gamma=1, 
-				lambda.start=lambda[1],lambda.min.ratio=1e-4)
-	oosdev[k] <- getdev(fitk)
+	dmrfit <- dmr(NULL, 
+				fgl[-lo,1:9], fgl$type[-lo], lambda.min.ratio=1e-4)
+	cvdmrfit <- dmr(NULL, cv=TRUE,
+				fgl[-lo,1:9], fgl$type[-lo], lambda.min.ratio=1e-4)
+	cvnet <- cv.glmnet(as.matrix(fgl[-lo,1:9]), fgl$type[-lo], family="multinomial")
+
+	oosdev[k,"net.CV1se"] <- getdev(cvnet,lo,select="1se")
+	oosdev[k,"net.CVmin"] <- getdev(cvnet,lo,select="min")
+	oosdev[k,"dmr.AICc"] <- getdev(dmrfit,lo)
+	oosdev[k,"dmr.CV1se"] <- getdev(cvdmrfit,lo,select="1se")
+	oosdev[k,"dmr.CVmin"] <- getdev(cvdmrfit,lo,select="min")	
+
 	print(k)
 }
-cvm <- mean(oosdev)
-cvs <- sd(oosdev)/sqrt(K-1)
-cvlo <- cvm-cvs
-cvhi <- cvm+cvs
 
-library(glmnet)
-fld <- c(1,rep.int(1:K,times=diff(chunks)))
-fld[randi] <- fld
-cvnet <- cv.glmnet(as.matrix(fgl[,1:9]), fgl$type, 
-			family="multinomial",foldid=fld)
-
-pdf(file="fgl_cv.pdf", width=7,height=4)
-par(mai=c(.9,.9,.6,0))
-plot(cvnet, bty="n",ylim=c(1.5,3.5),xlim=c(-10.5,2),xaxt="n")
-axis(1,at=c(-10,-8,-6,-4,-2))
-lines(c(.5,.5),c(cvlo,cvhi),col="grey60",lwd=2)
-lines(c(0,1),c(cvlo,cvlo),col="grey60",lwd=2)
-lines(c(0,1),c(cvhi,cvhi),col="grey60",lwd=2)
-lines(c(.4,.6),c(cvm,cvm),col="darkblue",lwd=4)
-axis(1, at=1/2,labels="DMR")
+pdf(file="fgl_cv.pdf", width=7,height=3)
+par(mai=c(.5,.9,.2,.2))
+boxplot(oosdev, bty="n",col=c(rep("pink",2),rep("lightblue",3)),
+	ylab="multinomial deviance",labels=c("net.CV1se"))
 dev.off()
-
-
-
 
 
 
